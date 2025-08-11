3b497b6beb79b8959097c7d2c4bc4fef019c7cca
Owen Walton <odwalton07@gmail.com>
2025-08-11 18:50:23 +0100

    Edit comment

--- src/main/java/com/betwise/oddscalc/service/IngestionService.java
+++ src/main/java/com/betwise/oddscalc/service/IngestionService.java
@@ -31,7 +31,6 @@ public class IngestionService {
 
             CricketMatchDataSchema schema = cricSheetParser.parseMatchesBatch(batchIDs);
             uploadCricketMatchDataSchema(schema);
-            // schema is now GC-eligible
         }
     }
 
