be3fa5871399e4ecc3010be97621c849c25ad4e3
Owen Walton <odwalton07@gmail.com>
2025-09-25 09:14:31 +0100

    Program setup

--- java/src/main/java/com/betwise/oddscalc/main/MainApp.java
+++ java/src/main/java/com/betwise/oddscalc/main/MainApp.java
@@ -12,6 +12,6 @@ public class MainApp {
     public static void main(String[] args) throws IOException {
         Controller controller = new Controller();
 
-        controller.maintain();
+        controller.init();
     }
 }
