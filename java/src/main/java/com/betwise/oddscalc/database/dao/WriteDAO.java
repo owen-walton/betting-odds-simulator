9cbfbce40eb01c39ea590bcd78816c63f2497ec4
Owen Walton <odwalton07@gmail.com>
2025-11-12 09:47:50 +0000

    Update comment

--- java/src/main/java/com/betwise/oddscalc/database/dao/WriteDAO.java
+++ java/src/main/java/com/betwise/oddscalc/database/dao/WriteDAO.java
@@ -3,6 +3,6 @@ package com.betwise.oddscalc.database.dao;
 import java.util.List;
 
 public interface WriteDAO<T> {
-    public boolean insert(T t); // currently no duplicate handling
-    public void bulkInsertIfNotExists(List<T> t);
+    boolean insert(T t);
+    void bulkInsertIfNotExists(List<T> t);
 }
